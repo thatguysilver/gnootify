33430e82e4fe62801e61102c9909d7cd67fd9058
thatguysilver <adampaulsilver>
2017-08-23 06:09:13 -0400

    added to readme and gitignore

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 **CLI controller that takes suggestions from gnoosic.com and 
 adds them to the spotify playlist of your choosing.**
 
-~~~~$ python3.6 band1 band2 band3~~~~
+~~~~$ python3.6 'band1' 'band2' 'band3'~~~~
 
 Will add gnoosic.com's recommendations to the spotify playlist of your choosing.
+
+Currently creates a CLI list. Only works for one-word artist names.
+
--- main.py
+++ main.py
@@ -4,19 +4,19 @@ from bs4 import BeautifulSoup as bs
 import sys, requests
 import time
 
-def page_iter():
-    'cycles through the gnoosic.com pages'
+def gnoosic_iter():
+    'cycles through the gnoosic.com gnoosics'
     temp_list = []
     driver = webdriver.Firefox()
     driver.get('http://gnoosic.com/faves.php')
 
     if len(sys.argv) > 1:
         i = 1
-        #fave = ''
         for item in sys.argv[1:]:
             fave = driver.find_element_by_name(f'Fave0{i}')
             fave.send_keys(item)
             i += 1
+        print(item)
         fave.send_keys(Keys.RETURN)
 
     idk_list = []
@@ -25,14 +25,23 @@ def page_iter():
         try:
             driver.find_element_by_name('Rate00').click()
         except NoSuchElementException:
-            print('We\'re done here!')
+            print('something went wrong!')
     print('loop exited')
+
     driver.find_element_by_xpath('//tr[3]/td/form/table/tbody/tr[4]/td/input').click()
+
+    print('Here are your recommendations:')
+
+    time.sleep(10)
 
     for i in range(11):
         text = driver.find_element_by_xpath(f'//tr[3]/td/div[2]/div[2]/a[{i+1}]').text
-
         print(text)
+        idk_list.append(text)
+
+    return idk_list
+
+
 
 if __name__ == '__main__':
-    page_iter()
+    gnoosic_iter()
